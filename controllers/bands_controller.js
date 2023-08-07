7c1fe22d36dd3760ea41bff709d347cd5bc02155
Luddaa <dangeloeugene33@gmail.com>
2023-08-07 14:13:12 -0700

    finished all files

--- controllers/bands_controller.js
+++ controllers/bands_controller.js
@@ -1,7 +1,7 @@
 const bands = require('express').Router();
 const { Op } = require('sequelize');
 const db = require('../models');
-const { Band } = db;
+const { Band, MeetGreet, SetTime, Event } = db 
 
 //*get all band
 bands.get('/', async (req, res) => {
@@ -10,7 +10,7 @@ bands.get('/', async (req, res) => {
             order:[["available_start_time","ASC"]],
             where:{
                 name:{
-                    [Op.like]:`%${req.query.name}%`,
+                    [Op.like]:`%${req.query.name ? req.query.name: ''}%`,
                 },
             },
     });
@@ -21,16 +21,42 @@ bands.get('/', async (req, res) => {
 });
 
 //*get one band
-bands.get('/:id', async (req, res) => {
+bands.get('/:name', async (req, res) => {
     try {
         const foundBand = await Band.findOne({
-            where: { band_id: req.params.id }
+            where: { name: req.params.name },
+            include: [
+                { 
+                    model: MeetGreet, 
+                    as: "meet_greets", 
+                    attributes: { exclude: ["band_id", "event_id"] },
+                    include: { 
+                        model: Event, 
+                        as: "event", 
+                        where: { name: { [Op.like]: `%${req.query.event ? req.query.event : ''}%` } } 
+                    }
+                },
+                { 
+                    model: SetTime, 
+                    as: "set_times",
+                    attributes: { exclude: ["band_id", "event_id"] },
+                    include: { 
+                        model: Event, 
+                        as: "event", 
+                        where: { name: { [Op.like]: `%${req.query.event ? req.query.event : ''}%` } } 
+                    }
+                }
+            ],
+            order: [
+                [{ model: MeetGreet, as: "meet_greets" }, { model: Event, as: "event" }, 'date', 'DESC'],
+                [{ model: SetTime, as: "set_times" }, { model: Event, as: "event" }, 'date', 'DESC']
+            ]
         })
         res.status(200).json(foundBand)
-    } catch(e) {
-        res.status(500).json(e)
-        }
-    })
+    } catch (error) {
+        res.status(500).json(error)
+    }
+})
 
     //*create band
     bands.post('/', async (req, res) => {
--- models/stage.js
+++ models/stage.js
@@ -1,39 +1,44 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
+const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class Stage extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  }
-  Stage.init({
-    stage_id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true
-    },
-    stage_name: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    capacity: {
-      type: DataTypes.INTEGER,
-      allowNull: false
-    }
-  }, {
-    sequelize,
-    modelName: 'Stage',
-    tableName: 'stage',
-    timestamps: false
-  });
-  return Stage;
+	class Stage extends Model {
+
+		static associate({ Event, StageEvent, SetTime }) {
+			Stage.belongsToMany(Event, {
+				foreignKey: 'stage_id',
+				as: 'events',
+				through: StageEvent,
+			});
+			Stage.hasMany(SetTime, {
+				foreignKey: 'stage_id',
+				as: 'set_times',
+			});
+		}
+	}
+	Stage.init(
+		{
+			stage_id: {
+				type: DataTypes.INTEGER,
+				primaryKey: true,
+				autoIncrement: true,
+			},
+			stage_name: {
+				type: DataTypes.STRING,
+				allowNull: false,
+			},
+			capacity: {
+				type: DataTypes.INTEGER,
+				allowNull: false,
+			},
+		},
+		{
+			sequelize,
+			modelName: 'Stage',
+			tableName: 'stage',
+			timestamps: false,
+		}
+	);
+	return Stage;
 };
 
 //:sequelize model:generate --name Stage --attributes "stage_id:integer, name:string, capacity:integer"
